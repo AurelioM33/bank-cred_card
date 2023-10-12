d993709bd2d619de696511fec432697a20077f30
AurelioM33 <m.aurelio887@gmail.com>
2023-10-12 00:01:51 -0300

    Image.network/Icons

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -45,9 +45,13 @@ class HomeScreen extends StatelessWidget {
         padding: const EdgeInsets.all(24.0),
         child: Column(
           children: [
+            // search field
             Container(
               height: 40,
-              decoration: BoxDecoration(color: Colors.grey.shade200),
+              decoration: BoxDecoration(
+                color: Colors.grey.shade200,
+                borderRadius: BorderRadius.circular(8),
+              ),
               child: TextField(
                 decoration: InputDecoration(
                   prefixIcon: Icon(
@@ -65,6 +69,90 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
             ),
+            //Card
+            const SizedBox(height: 32),
+            Image.network(
+              "https://www.itau.com.br/media/dam/m/3671f420a6735d7e/original/itaucard-uniclass-mastercard-black-png.png",
+            ),
+
+            const SizedBox(height: 32),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                //Scan
+                Column(
+                  children: [
+                    Container(
+                      width: 64,
+                      height: 64,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 1,
+                          color: Colors.grey.shade300,
+                        ),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Card(
+                        child: Icon(Icons.qr_code_scanner,
+                            color: Colors.blue.shade500),
+                      ),
+                    ),
+                    const SizedBox(height: 2),
+                    const Text("Scan"),
+                  ],
+                ),
+
+                //Send & pay
+
+                Column(
+                  children: [
+                    Container(
+                      width: 64,
+                      height: 64,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 1,
+                          color: Colors.grey.shade300,
+                        ),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Card(
+                        child: Icon(Icons.send, color: Colors.blue.shade500),
+                      ),
+                    ),
+                    const SizedBox(height: 2),
+                    const Text("Sand & Pay"),
+                  ],
+                ),
+
+                //Move Balance
+
+                Column(
+                  children: [
+                    Container(
+                      width: 64,
+                      height: 64,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 1,
+                          color: Colors.grey.shade300,
+                        ),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Card(
+                        child: Icon(Icons.control_camera,
+                            color: Colors.blue.shade500),
+                      ),
+                    ),
+                    const SizedBox(height: 2),
+                    const Text("Move Balance"),
+                  ],
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 32),
+            const Text("In & Out"),
           ],
         ),
       ),
